783ea22211ad431b9594363ff4fc0be92a83aa1e
alex-uc3m <00481003@alumnos.uc3m.es>
2022-06-30 00:09:12 -0700

    Progress

--- src/App.js
+++ src/App.js
@@ -1,47 +1,73 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 import Experience from "./Experience";
 
 const App = () => {
-  const experiences = [
+  const work_experiences = [
     {
-      title: "Software Engineer",
+      title: "Microsoft",
+      year: "2022",
+      description:
+        "I am working at Microsoft as a Product Manager Intern, championing the sustainability efforts of the SiGMa Media Team.",
+    },
+    {
+      title: "MizMaa Ventures",
+      year: "2021",
+      description: "I am a software engineer at a startup company.",
+    },
+    {
+      title: "IBM",
       year: "2020",
       description: "I am a software engineer at a startup company.",
     },
     {
-      title: "Software Engineer",
-      year: "2020",
+      title: "Ring",
+      year: "2017",
+      description: "I am a software engineer at a startup company.",
+    },
+  ];
+
+  const other_experiences = [
+    {
+      title: "iOS App Shoo",
+      year: "2021",
       description: "I am a software engineer at a startup company.",
     },
     {
-      title: "Software Engineer",
-      year: "2020",
+      title: "a&b granola",
+      year: "2012-19",
       description: "I am a software engineer at a startup company.",
     },
     {
-      title: "Software Engineer",
-      year: "2020",
-      description: "I am a software engineer at a startup company.",
-    },
-    {
-      title: "Software Engineer",
-      year: "2020",
+      title: "Coding School",
+      year: "2016-19",
       description: "I am a software engineer at a startup company.",
     },
   ];
 
   return (
     <div className="App">
-      {/* <div className="top-block"> */}
-      {/* <div className="name-and-hello">
-          <h1>
-            Alexander<br></br>Schiff
-          </h1>
-        </div> */}
+      <div className="name-and-hello">
+        <h1>
+          <RoughNotation
+            type="bracket"
+            multiline={false}
+            padding={10}
+            strokeWidth={8}
+            brackets={["left", "right"]}
+            show={true}
+            color={["#ECB02A"]}
+          >
+            Alexander
+            <br></br>
+            Schiff
+          </RoughNotation>
+        </h1>
+      </div>
       <div className="experiences">
         <ul>
-          {experiences.map((experience) => {
+          {work_experiences.map((experience) => {
             return (
               <Experience
                 title={experience.title}
@@ -52,7 +78,25 @@ const App = () => {
           })}
         </ul>
       </div>
-      {/* </div> */}
+
+      <br></br>
+      <br></br>
+      <br></br>
+
+      <div className="experiences">
+        <ul>
+          {other_experiences.map((experience) => {
+            return (
+              <Experience
+                title={experience.title}
+                year={experience.year}
+                description={experience.description}
+              />
+            );
+          })}
+        </ul>
+      </div>
+      <div className="bottom"></div>
     </div>
   );
 };
--- src/Experience.js
+++ src/Experience.js
@@ -1,14 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-const Experience = ({ title, year, description }) => {
+const Experience = ({ title, year, description, open }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="experience">
       <div className="item">
-        <h3 className="title">{title}</h3>
+        <h3
+          className="title"
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+        >
+          {title} {isOpen ? "<" : ">"}
+        </h3>
         <h3 className="year">{year}</h3>
       </div>
-      <hr></hr>
+      {isOpen && <p className="description">{description}</p>}
+      {/* <hr></hr> */}
     </div>
   );
 };
